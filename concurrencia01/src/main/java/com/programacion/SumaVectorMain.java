44b520494405bc5f44101c9727531c403e114f8d
kellyss1 <kdledesma@uce.edu.ec>
2025-01-28 16:42:16 -0500

    Concurrencia modificada

--- concurrencia01/src/main/java/com/programacion/SumaVectorMain.java
+++ concurrencia01/src/main/java/com/programacion/SumaVectorMain.java
@@ -21,19 +21,10 @@ public class SumaVectorMain {
             } return resultado;
         }
 
-        public void agregarSuma() {
-            sumas.add(0);
-        }
-    }
-
-//    static Integer sumaSerial(int[] datos) {
-//        int suma = 0;
-//        for (int i = 0; i < datos.length; i++) {
-//            suma = suma+datos[i];
+//        public void agregarSuma() {
+//            sumas.add(0);
 //        }
-//        return suma;
-//    }
-
+    }
 
     static Integer sumaThreads(int[] datos, int tasks) throws Exception{
 
@@ -41,24 +32,20 @@ public class SumaVectorMain {
         for(int i=0; i < tasks; i++){
             task.add(new SumaTask(datos, i*(datos.length/tasks), (i+1)*(datos.length/tasks)));
         }
-//
-//        int octavo = datos.length / 8;
-//
-//        SumaTask task1 = new SumaTask(datos,0, octavo);
-//        SumaTask task2 = new SumaTask(datos,octavo, octavo*2);
-//        SumaTask task3 = new SumaTask(datos,octavo*2, octavo*3);
-
+        if(datos.length % tasks != 0){
+            task.get(tasks-1).setEnd(datos.length);
+        }
 
         final ResultHolder result = new ResultHolder(tasks);
 
         ArrayList<Thread> threads = new ArrayList<>();
-        if(datos.length % tasks != 0){
-            int modulo = datos.length % tasks;
-            task.add(new SumaTask(datos, datos.length-modulo, datos.length));
-            System.out.println("Se ha creado un hilo adicional, ya que el tamaño del vector no es divisible entre el número de tareas");
-            tasks++;
-            result.agregarSuma();
-        }
+//        if(datos.length % tasks != 0){
+//            int modulo = datos.length % tasks;
+//            task.add(new SumaTask(datos, datos.length-modulo, datos.length));
+//            System.out.println("Se ha creado un hilo adicional, ya que el tamaño del vector no es divisible entre el número de tareas");
+//            tasks++;
+//            result.agregarSuma();
+//        }
         for(int i=0; i < tasks; i++){
             int finalI = i;
             threads.add(new Thread(() -> {result.sumas.set(finalI, task.get(finalI).call());}));
@@ -72,7 +59,6 @@ public class SumaVectorMain {
         });
 
         return result.sumResults();
-        //return result.suma1 + result.suma2+ result.suma3+ result.suma4+ result.suma5+ result.suma6+ result.suma7+ result.suma8;
     }
 
     static class SumaTask implements Callable<Integer> {
@@ -86,6 +72,9 @@ public class SumaVectorMain {
             this.end = end;
         }
 
+        public void setEnd(int end) {
+            this.end = end;
+        }
 
         @Override
         public Integer call() {
@@ -113,8 +102,89 @@ public class SumaVectorMain {
             vector[i] = i;
         }
 
-        int suma = sumaThreads(vector, 4);
+        int suma = sumaThreads(vector, 3);
         System.out.println("Suma total: " + suma);
     }
 
 }
+
+//public class SumaVectorMain {
+//
+//    static class ResultHolder{
+//        Integer suma1 = 0;
+//        Integer suma2 = 0;
+//
+//    }
+//
+//    static Integer sumaSerial(int[] datos) {
+//        int suma = 0;
+//        for (int i = 0; i < datos.length; i++) {
+//            suma = suma+datos[i];
+//        }
+//        return suma;
+//    }
+//
+//    static Integer sumaThreads(int[] datos) throws Exception{
+//
+//        int mitad = datos.length / 2;
+//
+//        SumaTask task1 = new SumaTask(datos,0, mitad);
+//        SumaTask task2 = new SumaTask(datos, mitad, datos.length);
+//
+//        final ResultHolder result = new ResultHolder();
+//
+//        Thread t0 = new Thread(() -> {result.suma1 = task1.call(); } );
+//        Thread t1 = new Thread(() -> {result.suma2 = task2.call(); } );
+//
+//        t0.start();
+//        t1.start();
+//
+//        t0.join();
+//        t1.join();
+//
+//        return result.suma1 + result.suma2;
+//    }
+//
+//    static class SumaTask implements Callable<Integer> {
+//        private int[] data;
+//        private int start;
+//        private int end;
+//
+//        SumaTask(int[] data, int start, int end){
+//            this.data = data;
+//            this.start = start;
+//            this.end = end;
+//        }
+//
+//
+//        @Override
+//        public Integer call() {
+//            System.out.printf("[%s] inicio Calculando suma, start%d, end%d\n" , Thread.currentThread().getName(), start, end);
+//            try{
+//                Thread.sleep(10_000);
+//            }catch (Exception e){
+//            }
+//            int suma = 0;
+//            for (int i=start; i<end; i++){
+//                suma = suma + data[i];
+//            }
+//            System.out.printf("[%s] Fin calculando suma, start%d, end%d\n" , Thread.currentThread().getName(),start, end);
+//            return suma;
+//        }
+//    }
+//
+//
+//
+//    public static void main(String[] args) throws Exception{
+//        //[0,1,2,3,4,..........99]
+//        int[] vector = new int[1_000];
+//
+//        for (int i = 0; i < vector.length; i++) {
+//            vector[i] = i;
+//        }
+//
+//        int suma = sumaThreads(vector);
+//        System.out.println("Suma total: " + suma);
+//    }
+//
+//}
